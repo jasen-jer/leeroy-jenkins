99518d90bc1d12626e4e815ace26b23c0e7142dc
JasenABaker <JasenABaker@gmail.com>
2018-05-26 15:55:09 -0400

    Started to create a contact us form for automatic email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 
 // probably needs to be stateful instead of stateless
 class Contact extends Component {
+    state = {
+        name: '',
+        email: '',
+        message: '',
+    }
   render() {
     return (
       <div className="blue-see-thru">
-        Hello from Contact
+      <h2>Contact Us</h2>
+      <p>We would love to hear from you and about your project.</h2>
+       <form method="POST">
+       <label htmlFor="name">Name</label>
+    <input type="text" name="name" />
+
+    <label htmlFor="email">Email</label>
+    <input type="email" name="email" />
+
+    <label htmlFor="message">Message</label>
+    <textarea name="message" rows="3"></textarea>
+
+    <input type="submit" />
+  </form>
         
       </div>
     );
