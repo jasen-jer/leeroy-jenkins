885830276fbb77d2e6fd9e4e9d1624b20d7e363b
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-05-31 13:46:59 -0400

    Reformatted project cards and overall layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,41 +3,61 @@ import React from 'react';
 
 const Projects = () => (
 
-  <div className="projects-wrapper">
+  <div className="projects-wrapper row">
 
-    <div className="all-projects-card">
-      <div className="project-card">
-        <a className="project-link" href="https://hair-scheduler.herokuapp.com/">
-          <h3 className="project-title">H.A.I.R</h3>
-          <img className="project-img" src="https://i.imgur.com/eYQZg5j.png?1" alt="Hair Appointment scheduling app" />
-          <p>Most hair stylists still use a paper and pen appointment book
-          so I decided to make a scheduling app.
-          The main idea behind the app is to have access to your appointments from anywhere.
-          Built using React, Ruby on Rails, and PostgreSQL.
-          </p>
-        </a>
-      </div>
-      <div className="project-card">
-        <a className="project-link" href="https://warhol-inv.herokuapp.com/">
-          <h3 className="project-title">Art Inventory Manager</h3>
-          <img className="project-img" src="https://i.imgur.com/q4jzsXh.png?1" alt="Western Hangman" />
-          <p>In this app, you play the part of a prestigious art dealer. After creating your user account, 
+        <div className="col s12 m6 l4">
+          <div className="card large">
+            <div className="card-image">
+              <img src="https://i.imgur.com/Apqxbbk.jpg" alt="Hair Appointment scheduling app" />
+              <span className="card-title black-text">H.A.I.R</span>
+            </div>
+            <div className="card-content">
+              <p>Most hair stylists still use a paper and pen appointment book
+              so I decided to make a scheduling app.
+              The main idea behind the app is to have access to your appointments from anywhere.
+              Built using React, Ruby on Rails, and PostgreSQL.
+              </p>
+            </div>
+            <div className="card-action">
+              <a href="https://hair-scheduler.herokuapp.com/">Visit project</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col s12 m6 l4">
+          <div className="card large">
+            <div className="card-image">
+              <img src="https://i.imgur.com/UkUOmdn.png" alt="Art Inventory Manager" />
+              <span className="card-title black-text">Art Inventory Manager</span>
+            </div>
+            <div className="card-content">
+              <p>In this app, you play the part of a prestigious art dealer. After creating your user account, 
             you can create an art gallery (or several galleries) 
             and then create or select paintings to display in your new space. 
-          </p>
-        </a>
-      </div>
-      <div className="project-card">
-        <a className="project-link" href="https://young-bastion-12479.herokuapp.com/">
-          <h3 className="project-title">Jasen's app</h3>
-          <img className="project-img" src="https://i.imgur.com/dZsLQ5Z.png?1" alt="Tree Time app" />
-          <p>I am Singing Wind, Chief of the Martians. Anyhoo, your net-suits will allow you to experience Fry's worm infested bowels as if you were actually wriggling through them. Incidentally, you have a dime up your nose.
-Robot 1-X, save my friends! And Zoidberg! Well, then good news! It's a suppository. Moving along… Good news, everyone! I've taught the toaster to feel love! Tell them I hate them.
-          </p>
-        </a>
-      </div>
+              </p>
+            </div>
+            <div className="card-action">
+              <a href="https://warhol-inv.herokuapp.com/">Visit project</a>
+            </div>
+          </div>
+        </div>
 
-    </div>
+        <div className="col s12 m6 l4">
+          <div className="card large">
+            <div className="card-image small">
+              <img src="https://i.imgur.com/OoMZYVL.png" alt="Tome adventure app" />
+              <span className="card-title black-text">Tome</span>
+            </div>
+            <div className="card-content">
+              <p>One app to rule them all, Tome allows you to run your dungeons and dragons adventures all from one app! 
+                Gone are the days of juggling multiple resources, flipping through books and notes to find the right piece of information for your game.
+              </p>
+            </div>
+            <div className="card-action">
+              <a href="https://tome-dm.herokuapp.com/">Visit project</a>
+            </div>
+          </div>
+        </div>
 
   </div>
 )
