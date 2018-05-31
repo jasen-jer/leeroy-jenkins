ee576ed5b7b7a1d0f7781163eda321825265e7aa
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-05-31 10:26:24 -0400

    Specified class names

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,9 +5,9 @@ const Projects = () => (
 
   <div className="projects-wrapper">
 
-    <div className="projects-div">
-      <div className="project-div">
-        <a className="styled-link" href="https://hair-scheduler.herokuapp.com/">
+    <div className="all-projects-card">
+      <div className="project-card">
+        <a className="project-link" href="https://hair-scheduler.herokuapp.com/">
           <h3 className="project-title">H.A.I.R</h3>
           <img className="project-img" src="https://i.imgur.com/eYQZg5j.png?1" alt="Hair Appointment scheduling app" />
           <p>Most hair stylists still use a paper and pen appointment book
@@ -17,8 +17,8 @@ const Projects = () => (
           </p>
         </a>
       </div>
-      <div className="project-div">
-        <a className="styled-link" href="https://warhol-inv.herokuapp.com/">
+      <div className="project-card">
+        <a className="project-link" href="https://warhol-inv.herokuapp.com/">
           <h3 className="project-title">Art Inventory Manager</h3>
           <img className="project-img" src="https://i.imgur.com/q4jzsXh.png?1" alt="Western Hangman" />
           <p>In this app, you play the part of a prestigious art dealer. After creating your user account, 
@@ -27,8 +27,8 @@ const Projects = () => (
           </p>
         </a>
       </div>
-      <div className="project-div">
-        <a className="styled-link" href="https://young-bastion-12479.herokuapp.com/">
+      <div className="project-card">
+        <a className="project-link" href="https://young-bastion-12479.herokuapp.com/">
           <h3 className="project-title">Jasen's app</h3>
           <img className="project-img" src="https://i.imgur.com/dZsLQ5Z.png?1" alt="Tree Time app" />
           <p>I am Singing Wind, Chief of the Martians. Anyhoo, your net-suits will allow you to experience Fry's worm infested bowels as if you were actually wriggling through them. Incidentally, you have a dime up your nose.
