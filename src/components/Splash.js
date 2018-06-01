aea8295850fd6bfa022a93fdc5893280eb2ba123
JasenABaker <JasenABaker@gmail.com>
2018-06-01 13:11:31 -0400

    Add scroll to down arrow on splash

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -55,9 +55,21 @@ const Splash = () => {
       </div>
       
       {/* scroll button */}
+      
       <div className="container-splash-bottom">
         <div>
+        <Link 
+                    activeClass= "Active"
+                    to="Mission"
+                    spy={true}
+                    smooth={true}
+                    hashSpy={true}
+                    offset={20}
+                    duration={1000}
+                    delay={30}
+                    isDynamic={true}>
           <i className="material-icons btn-splash-scroll">keyboard_arrow_down</i>
+          </Link>
         </div>
 
         <div className="bg-splash"></div>
